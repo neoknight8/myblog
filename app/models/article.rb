429ab981b3197cb5cfe2df17e09bf6e03ca9bddd
neoknight8 <kosukekomiya@hotmail.com>
2017-02-18 13:21:17 +0000

    fixed part2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
     has_many :comments, :dependent => :destroy
-    validates :title, presence: {message: "タイトルがありません。"}
-    validates :title, length: {maximum: 20,message: "タイトルはは20字以下にしてください。"}
-    validates :text, presence: {message: "本文がありません。"}    
+    validates :title, presence: true
+    validates :title, length: {maximum: 20}
+    validates :text, presence: true    
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :article_params
-  validates :title, presence: {message: "タイトルが入力されていません。"}
-  validates :text, presence: {message: "本文が入力されていません。"}
+  validates :title, presence: true
+  validates :text, presence: true
 
 end
